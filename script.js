9e6e009b4235c8102584552085877fbe88ea460c
Bhavya Luthra <bhavyaluthra1234@gmail.com>
2025-10-23 16:58:50 +0530

    Script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+/** HTML
+ * !DOCTYPE -  <!DOCTYPE html> - declares the document type and tells the browser which the version of html 
+ * - Block and inline elements - BLock elements are those elements that take up the whole space like <div> , <h1> , <p> where the inline elements comes in one line like <img> <span> <i> 
+ * - Tags and Attributes - 
+ *  - Head and Title 
+ * - imports (script , style... )
+ *  - HEADINGS 
+ * - TABLES 
+ * - ANCHORS AND NAVIGATION
+ *  - LISTS
+ * - Forms And Input types
+ * - Events basics
+ * - Images
+ * - Semantic HTML
+ *  - Local & Session Storage
+
+!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */
