3140793aec33e0d337466e3c539f0477e72cbcfe
Bhavya Luthra <bhavyaluthra1234@gmail.com>
2025-10-28 16:42:58 +0530

    Controlled and UnControlled Components

--- script.js
+++ script.js
@@ -2,21 +2,54 @@
  * !DOCTYPE -  <!DOCTYPE html> - declares the document type and tells the browser which the version of html 
  * - Block and inline elements - BLock elements are those elements that take up the whole space like <div> , <h1> , <p> where the inline elements comes in one line like <img> <span> <i> 
  * - Tags and Attributes - 
- *  - Head and Title 
+ *  - Head and Title - 
  * - imports (script , style... )
- *  - HEADINGS 
- * - TABLES 
- * - ANCHORS AND NAVIGATION
- *  - LISTS
+ *  - HEADINGS - 
+ * - TABLES -
+ * - ANCHORS AND NAVIGATION - ANCHOR TAGS IS BASICALLY THE A TAG THAT WE USE FOR LINKS OR LIKE FOR NAVIGATION PURPOSES IT ALSO HAVE ATTRIBUTES LIKE HREF WHERE THE LINK  GOES , TAGR
+ *  - LISTS -  ORDERED AND UNORDERED LISTS WHERE ORDERED LISTS ARE NUMBERED AND UNORDERED LISTS ARE BULLETED 
  * - Forms And Input types
  * - Events basics
  * - Images
  * - Semantic HTML
  *  - Local & Session Storage
 
-!
+ CSS
+ -  Selectors
+ - Basic 
+ - Combinational
+ - Psuedo
+ - selectors
+ - elements
+ - specificity
+ - inheritance
+ - Box Model
+  - Fonts and Typography 
+  - Colors
+  - Units (absolute + relative)
+  - Overflow
+  - Float
+  - Display and Flex
+ 
 
-
+  JAVASCRIPT
+  - Primitives
+  - Scopes & Hoisting
+  - Closures
+  - Execution Context 
+  - Variables
+   - Operations
+   - Type Conversions
+   - Arrays + methods 
+   - Objects + Methods
+   - Functions + Arrow Functions
+   - Try Catch and Error handling
+   - Strict mode 
+   - Timeout and Interval
+   - Classes
+   
+ - controlled components and uncontrolled components
+ - controlled components are basically the components where the react has control over  like state and props where as uncontrolled components are the components where the DOM has control over the data  like form data 
 
 
 
